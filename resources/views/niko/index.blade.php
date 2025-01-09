335b37dde209cff0cad7c9f19937ceb63ce940c7
niko685 <nikowinong2@gmail.com>
2025-01-09 09:25:36 +0700

    ncsjsjdj

--- resources/views/niko/index.blade.php
+++ resources/views/niko/index.blade.php
@@ -1,1 +1,3 @@
 Setiap langkah kecil yang kamu ambil hari ini adalah pondasi untuk mimpi besar di masa depan. Tetap semangat dan jangan pernah menyerah!
+
+Setiap langkah kecil yang kamu ambil hari ini adalah pondasi untuk mimpi besar di masa depan. Tetap semangat dan jangan pernah menyerah!
